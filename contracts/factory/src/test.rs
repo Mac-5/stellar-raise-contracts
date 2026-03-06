1dcd01810d847ee545867140892680c9fd0e5440
afurious <misraisiguzor@gmail.com>
2026-03-06 10:45:34 +0100

    fix: build crowdfund WASM before tests and use correct path

--- contracts/factory/src/test.rs
+++ contracts/factory/src/test.rs
@@ -6,7 +6,7 @@ extern crate std;
 // Import the crowdfund contract WASM.
 #[allow(clippy::too_many_arguments)]
 mod crowdfund_wasm {
-    soroban_sdk::contractimport!(file = "../wasm/crowdfund.wasm");
+    soroban_sdk::contractimport!(file = "../../target/wasm32-unknown-unknown/release/crowdfund.wasm");
 }
 
 fn create_token_contract<'a>(
